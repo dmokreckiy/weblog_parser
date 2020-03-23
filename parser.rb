e1ceb274b3ad2d5a510d7189cba0a5f26df52766
Denis Mokreckiy <d.mokreckiy@gmail.com>
2020-03-23 06:16:33 +0300

    Added execution time report and beutified output

--- parser.rb
+++ parser.rb
@@ -1,8 +1,29 @@
 #!/usr/bin/env ruby
 
-require_relative 'lib/validator'
-require_relative 'lib/pragmatic_parser'
+require 'monotonik'
+require_relative 'lib/parser_dependencies'
 
 Validator.new.call
-parser = PragmaticParser.new(ARGV[0])
-parser.call
+
+puts '*'*20
+puts 'Ruby Parser'
+puts '*'*20
+
+ruby_time = Monotonik.measure do
+  ruby_parser = RubyParser.new(ARGV[0])
+  ruby_parser.call
+end
+
+puts "Execution time - #{ruby_time.time}"
+
+puts '*'*20
+puts 'Unix Parser'
+puts '*'*20
+
+pragmatic_time = Monotonik.measure do
+  pragmatic_parser = PragmaticParser.new(ARGV[0])
+  pragmatic_parser.call
+end
+
+puts "Execution time - #{pragmatic_time.time}"
+
